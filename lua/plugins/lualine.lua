eb73cb4af18fea9a886aed5b300f01d50596c11f
Marcial Machado <marcialmachado0522@gmail.com>
2023-12-29 21:20:12 -0500

    Add hostname to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,14 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "auto"
-            }
-        })
-    end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto",
+			},
+			sections = {
+				lualine_x = { "hostname", "encoding", "fileformat", "filetype" },
+			},
+		})
+	end,
 }
