10bfc1acaa354b4fd27b02b87810d88c6d7afe4f
Marcial Machado <marcialmachado0522@gmail.com>
2024-01-03 15:12:06 -0500

    Add shortcuts to enable/disable twilight plugin

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -1,3 +1,7 @@
 return {
 	"folke/twilight.nvim",
+  config = function()
+    vim.keymap.set('n', '<leader>te', ':TwilightEnable<CR>')
+    vim.keymap.set('n', '<leader>td', ':TwilightDisable<CR>')
+  end
 }
