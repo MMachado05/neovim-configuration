acbcf2e942a78992d7a57f3499869dab2a13cb72
MMachado05 <marcialmachado0522@gmail.com>
2024-08-06 18:43:57 -0400

    Add buffer toggle with Telescope

--- lua/plugins/telescope-file-browser.lua
+++ lua/plugins/telescope-file-browser.lua
@@ -3,6 +3,6 @@ return {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
     config = function()
-        vim.keymap.set('n', '<leader>fb', command, { noremap = true })
+        vim.keymap.set('n', '<leader>fs', command, { noremap = true })
     end
 }
