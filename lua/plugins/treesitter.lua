49cfc037d4de1db8a504fbf3199bab1250dbb063
MMachado05 <marcialmachado0522@gmail.com>
2024-08-06 18:45:06 -0400

    Remove autoinstall
    Broke the hell out of my Vimtex. Better to just have it list out the
    *exact* ones that I want.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,8 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      auto_install = true,
+      auto_install = false,
+      ensure_installed = {"vim", "vimdoc", "markdown", "python", "java", "c", "lua"},
       highlight = { enable = true },
       indent = { enable = true },
       fold = { enable = true },
