16bcb7919c7e4c8173e68d49d3e8c9f5ac2c3185
MMachado05 <marcialmachado0522@gmail.com>
2024-08-08 18:49:15 -0400

    Improve git module

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,16 @@
+--  _____ _ _     _____                              _
+-- |  __ (_) |   /  ___|                            | |
+-- | |  \/_| |_  \ `--. _   _ _ __  _ __   ___  _ __| |_
+-- | | __| | __|  `--. \ | | | '_ \| '_ \ / _ \| '__| __|
+-- | |_\ \ | |_  /\__/ / |_| | |_) | |_) | (_) | |  | |_
+--  \____/_|\__| \____/ \__,_| .__/| .__/ \___/|_|   \__|
+--                           | |   | |
+--                           |_|   |_|
+-- Plugins that allow neovim to understand Git projects
+
 return {
 	"lewis6991/gitsigns.nvim",
-  config = function()
-    require('gitsigns').setup()
-  end
+	config = function()
+		require("gitsigns").setup()
+	end,
 }
