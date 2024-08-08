b3fa02dc41469c51d418ee482c55661e7ce2547e
MMachado05 <marcialmachado0522@gmail.com>
2024-08-08 19:33:51 -0400

    Change telescope to use the newest branch release, rather than a hardcoded release version

--- lua/plugins/file_movement.lua
+++ lua/plugins/file_movement.lua
@@ -26,7 +26,7 @@ return {
 	--  had each one explicitly, in case I need to make config changes in the future.
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
+		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local telescope_builtin = require("telescope.builtin")
