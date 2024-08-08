773293afb6c5de5223c0db5cf32015c5445f351b
MMachado05 <marcialmachado0522@gmail.com>
2024-08-08 17:48:14 -0400

    Move code-editing related plugins to module

--- lua/plugins/code_editing.lua
+++ lua/plugins/code_editing.lua
@@ -7,3 +7,38 @@
 --                                                  __/ |
 --                                                 |___/
 -- Plugins that make the general typing workflow nicer
+
+return {
+	{
+		"tpope/vim-surround",
+	},
+	{
+		"m4xshen/autoclose.nvim",
+		config = function()
+			require("autoclose").setup({
+				keys = {
+					["("] = { escape = false, close = true, pair = "()" },
+					["["] = { escape = false, close = true, pair = "[]" },
+					["{"] = { escape = false, close = true, pair = "{}" },
+
+					[">"] = { escape = true, close = false, pair = "<>" },
+					[")"] = { escape = true, close = false, pair = "()" },
+					["]"] = { escape = true, close = false, pair = "[]" },
+					["}"] = { escape = true, close = false, pair = "{}" },
+
+					['"'] = { escape = true, close = true, pair = '""' },
+					["'"] = { escape = true, close = true, pair = "''" },
+					["`"] = { escape = true, close = true, pair = "``" },
+				},
+				options = {
+					disabled_filetypes = { "text" },
+					disable_when_touch = false,
+					touch_regex = "[%w(%[{]",
+					pair_spaces = false,
+					auto_indent = true,
+					disable_command_mode = false,
+				},
+			})
+		end,
+	},
+}
