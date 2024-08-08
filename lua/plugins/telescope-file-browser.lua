d91937f0b81eaccf4a10129a6013eccd0496b92d
MMachado05 <marcialmachado0522@gmail.com>
2024-08-08 18:49:35 -0400

    Create file_movement module

--- lua/plugins/telescope-file-browser.lua
+++
@@ -1,8 +0,0 @@
-local command = "<cmd>lua require 'telescope'.extensions.file_browser.file_browser({ hidden = true })<CR>"
-return {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    config = function()
-        vim.keymap.set('n', '<leader>fs', command, { noremap = true })
-    end
-}
