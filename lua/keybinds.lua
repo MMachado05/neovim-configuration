bfeaf5c0f8f9a75eaf9f1845b54648b7e5e550f2
MMachado05 <marcialmachado0522@gmail.com>
2024-08-09 11:10:45 -0400

    Move Telescope keybinds to dedicated file
    FINALLY!

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -61,3 +61,7 @@ vim.keymap.set("n", "<leader>te", ":TwilightEnable<CR>")
 vim.keymap.set("n", "<leader>td", ":TwilightDisable<CR>")
 
 -- Telescope
+local telescope_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
