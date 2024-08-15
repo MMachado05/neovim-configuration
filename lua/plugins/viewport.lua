2ed85f8c539ba6f3c20eebfee2837575877f7fd2
MMachado05 <marcialmachado0522@gmail.com>
2024-08-15 16:35:24 -0400

    Add nvim-notify plugin

--- lua/plugins/viewport.lua
+++ lua/plugins/viewport.lua
@@ -85,6 +85,17 @@ return {
     end,
   },
 
+  -- Prettying up existing features
+  {
+    -- Nicer notifications
+    "rcarriga/nvim-notify",
+    config = function()
+      require("notify").setup({
+        max_width = 50,
+      })
+    end,
+  },
+
   -- ---------------
   -- Visual Elements
   -- ---------------
