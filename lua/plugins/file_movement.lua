90bc997b834ba74c526cf4c0160d9df1b25ce8c8
MMachado05 <marcialmachado0522@gmail.com>
2024-08-15 16:35:47 -0400

    Fix formatting

--- lua/plugins/file_movement.lua
+++ lua/plugins/file_movement.lua
@@ -9,128 +9,59 @@
 -- How I move through files on my filesystem, or files open as buffers
 
 return {
-  -- --------------------------------
-  -- Oil for the RC thing or whatever
-  -- --------------------------------
-  {
-    "stevearc/oil.nvim",
-    opts = {},
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
+	-- -------------
+	-- Oil for netrw
+	-- -------------
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 
-  -- ---------
-  -- Telescope
-  -- ---------
-  -- TODO: I need to get rid of all dependencies. I think it'd just be better if I
-  --  had each one explicitly, in case I need to make config changes in the future.
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    -- Sets the Telescope UI to everything relating to selection
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
-  {
-    -- File browser with Telescope
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local command = "<cmd>lua require 'telescope'.extensions.file_browser.file_browser({ hidden = true })<CR>"
-      vim.keymap.set("n", "<leader>fs", command, { noremap = true })
-    end,
-  },
+	-- ---------
+	-- Telescope
+	-- ---------
+	-- TODO: I need to get rid of all dependencies. I think it'd just be better if I
+	--  had each one explicitly, in case I need to make config changes in the future.
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		-- Sets the Telescope UI to everything relating to selection
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+	{
+		-- File browser with Telescope
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local command = "<cmd>lua require 'telescope'.extensions.file_browser.file_browser({ hidden = true })<CR>"
+			vim.keymap.set("n", "<leader>fs", command, { noremap = true })
+		end,
+	},
 
-  -- -------
-  -- Harpoon
-  -- -------
-  -- TODO: Change this to arrow?
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      local harpoon = require("harpoon")
-      harpoon.setup({})
-
-      -- Allow Harpoon to use Telescope
-      local conf = require("telescope.config").values
-      local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
-        end
-
-        require("telescope.pickers")
-            .new({}, {
-              prompt_title = "Harpoon",
-              finder = require("telescope.finders").new_table({
-                results = file_paths,
-              }),
-              previewer = conf.file_previewer({}),
-              sorter = conf.generic_sorter({}),
-            })
-            :find()
-      end
-
-      -- Harpoon functionality keymaps
-      vim.keymap.set("n", "<leader>ho", function()
-        toggle_telescope(harpoon:list())
-      end, { desc = "Open harpoon window" })
-      vim.keymap.set("n", "<leader>ha", function()
-        harpoon:list():append()
-      end)
-      vim.keymap.set("n", "<leader>hp", function()
-        harpoon:list():prev()
-      end)
-      vim.keymap.set("n", "<leader>hn", function()
-        harpoon:list():next()
-      end)
-
-      vim.keymap.set("n", "<leader>hq", function()
-        harpoon:list():select(1)
-      end)
-      vim.keymap.set("n", "<leader>hw", function()
-        harpoon:list():select(2)
-      end)
-      vim.keymap.set("n", "<leader>he", function()
-        harpoon:list():select(3)
-      end)
-      vim.keymap.set("n", "<leader>hr", function()
-        harpoon:list():select(4)
-      end)
-
-      vim.keymap.set("n", "<leader>hc", function()
-        harpoon:list():clear()
-      end)
-
-      vim.keymap.set("n", "<leader>hdq", function()
-        harpoon:list():removeAt(1)
-      end)
-      vim.keymap.set("n", "<leader>hdw", function()
-        harpoon:list():removeAt(2)
-      end)
-      vim.keymap.set("n", "<leader>hde", function()
-        harpoon:list():removeAt(3)
-      end)
-      vim.keymap.set("n", "<leader>hdr", function()
-        harpoon:list():removeAt(4)
-      end)
-    end,
-  },
+	-- ---------------------
+	-- Arrow for bookmarking
+	-- ---------------------
+	{
+		"otavioschwanck/arrow.nvim",
+		opts = {
+			show_icons = true,
+			leader_key = ";", -- Recommended to be a single key
+			buffer_leader_key = "m", -- Per Buffer Mappings
+		},
+	},
 }
