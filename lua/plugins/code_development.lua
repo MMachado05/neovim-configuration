2a4f7814b1d37cc2ac87de9d62c06376a0d6e593
MMachado05 <marcialmachado0522@gmail.com>
2025-05-25 22:08:51 -0400

    Add tiny-inline-diagnostic (BROKEN)
    Nothing shows up, probably conflicting behaviors somewhere

--- lua/plugins/code_development.lua
+++ lua/plugins/code_development.lua
@@ -326,6 +326,15 @@ return {
     cmd = "Trouble",
     -- TODO: I need to read up on the documentation for this (and honestly, all of my plugins)
   },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "LspAttach", -- Or `LspAttach`
+    priority = 1000,    -- needs to be loaded in first
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+      vim.diagnostic.config({ virtual_text = false })   -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end
+  },
 
   -- -------------------------
   -- Language-specific plugins
